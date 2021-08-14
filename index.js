4ccce745a0f55581f786e437c142fc8b75cc1c10
Hyouka0193918 <88913597+Hyouka0193918@users.noreply.github.com>
2021-08-13 21:49:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+const { Client } = require('discord.js');
+const { readdir } = require('fs'); 
+
+const client = new Client();
+client.commands = new Map();
+
+readdir(__dirname + '/comandos', (error, arquivos) => {
+    if (error) throw error;
+    arquivos.filter(arquivo => arquivo.endsWith('.js')).forEach(arquivo => {
+        const comando = require('./comandos/' + arquivo);
+        comando.name.forEach(name => client.commands.set(name, comando.run));
+    });
+});
+
+readdir(__dirname + '/eventos', (error, arquivos) => {
+    if (error) throw error;
+    arquivos.filter(arquivo => arquivo.endsWith('.js')).forEach(arquivo => {
+        require('./eventos/' + arquivo)(client);
+    });
+});
+
+client.login('IUHZhykIUkUd3zE3vQdUHTv8yR9cJ5dp');
